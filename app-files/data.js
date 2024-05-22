abdfd46dd0a931387f72455fde32691f49ec0149
diegoUE8 <zazze88@gmail.com>
2024-05-22 14:51:44 +0200

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,166 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-test-office",
+      "name": "test-office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3939974874893792,
+          "pitch": -0.1309507480254588,
+          "rotation": 0,
+          "target": "1-test-library"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5071137331856619,
+          "pitch": 0.5464155873774619,
+          "title": "INFOPOINT",
+          "text": "TESTO"
+        },
+        {
+          "yaw": -1.58582516111111,
+          "pitch": 0.26544857366223873,
+          "title": "INFOPOINT",
+          "text": "TESTO"
+        }
+      ]
+    },
+    {
+      "id": "1-test-library",
+      "name": "test-library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.2140611371582821,
+          "pitch": 0.1900081195955483,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.7434044599301544,
+          "pitch": 0.17592274737119595,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3104583955754894,
+          "pitch": -0.02851762120269896,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.8010437786583928,
+          "pitch": 0.20437253357300378,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-adobestock_320609754",
+      "name": "AdobeStock_320609754",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2736,
+      "initialViewParameters": {
+        "yaw": -0.3235661806152539,
+        "pitch": 0.198774530690617,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.136497983645807,
+          "pitch": 0.39602449398957873,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.92923579757087,
+          "pitch": 0.2040984267089918,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Test360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
